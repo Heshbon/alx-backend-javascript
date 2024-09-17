0b835dc6144fcf48837f171f900e22770f60e2e8
Heshbon <cityalight.hesbon@gmail.com>
2024-09-17 14:55:55 +0000

    index.js

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,7 +1,11 @@
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
 
-
+/**
+ * Connect routes to the appropriate handler in the
+ * @param {Express} app The Express application.
+ * @throws {Error} Throws an error if the file cannot be loaded
+ */
 const mapRoutes = (app) => {
   app.get('/', AppController.getHomepage);
   app.get('/students', StudentsController.getAllStudents);
